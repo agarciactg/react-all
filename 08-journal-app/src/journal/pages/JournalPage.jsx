404977d5592b7e0abe4dc29559340082d49f8d38
Andrés García <agarciactg@gmail.com>
2024-03-19 17:15:22 -0500

    Navbar - SideBar.

--- 08-journal-app/src/journal/pages/JournalPage.jsx
+++ 08-journal-app/src/journal/pages/JournalPage.jsx
@@ -1,11 +1,13 @@
-import { MailOutline } from "@mui/icons-material"
+// import { MailOutline } from "@mui/icons-material"
 import { Typography } from "@mui/material"
+import { JournalLayout } from "../layout/JournalLayout"
+
 
 export const JournalPage = () => {
   return (
-    <>
-        <Typography variant="h1" >JournalPage</Typography>
-        <MailOutline/>
-    </>
+    <JournalLayout>
+        <Typography >El Lorem ipsum, como ya sabrás, es un texto utilizado habitualmente en diseño y maquetación, que permite comprobar que los espacios están bien distribuidos y las tipografías funcionan correctamente.</Typography>
+        {/* <MailOutline/> */}
+    </JournalLayout>
   )
 }
