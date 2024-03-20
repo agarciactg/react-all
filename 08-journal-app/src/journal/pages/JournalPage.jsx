30f2fc08e7b3dd65dce80a54648e4dedad6eb9fe
Andrés García <agarciactg@gmail.com>
2024-03-20 17:16:09 -0500

    ImagenList y Boton Flotante

--- 08-journal-app/src/journal/pages/JournalPage.jsx
+++ 08-journal-app/src/journal/pages/JournalPage.jsx
@@ -1,5 +1,8 @@
+import { IconButton } from "@mui/material"
 import { JournalLayout } from "../layout/JournalLayout"
+// import { NoteView } from "../views"
 import { NothingSelectedView } from "../views"
+import { AddOutlined } from "@mui/icons-material"
 
 
 export const JournalPage = () => {
@@ -8,6 +11,21 @@ export const JournalPage = () => {
         {/* <Typography >El Lorem ipsum, como ya sabrás, es un texto utilizado habitualmente en diseño y maquetación, que permite comprobar que los espacios están bien distribuidos y las tipografías funcionan correctamente.</Typography> */}
         {/* <MailOutline/> */}
         <NothingSelectedView/>
+        {/* <NoteView/> */}
+
+        <IconButton
+          size='large'
+          sx={{
+            color: 'white',
+            backgroundColor: 'error.main',
+            ':hover': { backgroundColor: 'error.main', opacity: 0.9 },
+            position: 'fixed',
+            right: 50,
+            bottom: 50
+          }}
+        >
+          <AddOutlined sx={{ fontSize: 30 }} />
+        </IconButton>
     </JournalLayout>
   )
 }
