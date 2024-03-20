d92b4e0352a4fcffd2da2b1f705ced0131a4a86b
Andrés García <agarciactg@gmail.com>
2024-03-20 14:37:35 -0500

    nothingSelectedView

--- 08-journal-app/src/journal/pages/JournalPage.jsx
+++ 08-journal-app/src/journal/pages/JournalPage.jsx
@@ -1,13 +1,13 @@
-// import { MailOutline } from "@mui/icons-material"
-import { Typography } from "@mui/material"
 import { JournalLayout } from "../layout/JournalLayout"
+import { NothingSelectedView } from "../views"
 
 
 export const JournalPage = () => {
   return (
     <JournalLayout>
-        <Typography >El Lorem ipsum, como ya sabrás, es un texto utilizado habitualmente en diseño y maquetación, que permite comprobar que los espacios están bien distribuidos y las tipografías funcionan correctamente.</Typography>
+        {/* <Typography >El Lorem ipsum, como ya sabrás, es un texto utilizado habitualmente en diseño y maquetación, que permite comprobar que los espacios están bien distribuidos y las tipografías funcionan correctamente.</Typography> */}
         {/* <MailOutline/> */}
+        <NothingSelectedView/>
     </JournalLayout>
   )
 }
