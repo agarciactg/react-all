8ff078a08596c315e4598738fcfac801125ac1e0
Andrés García <agarciactg@gmail.com>
2024-04-02 11:01:27 -0500

    axios

--- 09-toolkit-redux/src/store/slices/pokemon/thunks.js
+++ 09-toolkit-redux/src/store/slices/pokemon/thunks.js
@@ -1,10 +1,13 @@
-import { startLoadingPokemons } from "./pokemonSlice"
+import { pokemonApi } from "../../../api/pokemonApi";
+import { setPokemons, startLoadingPokemons } from "./pokemonSlice"
 
 export const getPokemons = ( page = 0 ) => {
-    return (dispatch, getState) => {
+    return async(dispatch, getState) => {
         dispatch( startLoadingPokemons());
 
         // TODO: realizar peticion http
-        fetch(`https://pokeapi.co/api/v2/pokemon?limit=10&offset=${ page * 10}`)
+        const data = await pokemonApi.get(`pokemon?limit=10&offset=${ page * 10}`);
+        
+        dispatch( setPokemons({ pokemons: data.results, page: page + 1 }) );
     }
 }
